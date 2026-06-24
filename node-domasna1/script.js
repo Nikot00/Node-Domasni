6d02dcea75f307033f1973b99f598c574d180b08
Nikot00 <103070034+Nikot00@users.noreply.github.com>
2026-06-24 18:44:18 +0200

    Add files via upload

---
+++ node-domasna1/script.js
@@ -0,0 +1,21 @@
+
+function moeIme() {
+     console.log("Nikola Davkovski");
+    
+}
+
+const imeMoe = function() {
+     console.log("Nikola Davkovski");
+    
+}
+
+myName = () => {
+     console.log("Nikola Davkovski");
+    
+}
+
+moeIme()
+
+imeMoe()
+
+myName()
